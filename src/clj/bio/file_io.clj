9bdcc300074847871543eb7a7e5d065e5abe3eed
nrakochy <nick.rakochy@gmail.com>
2016-08-03 13:21:03 -0500

    Added line-seq reducer fn. Test coverage for regex

--- src/clj/bio/file_io.clj
+++ src/clj/bio/file_io.clj
@@ -1,15 +1,18 @@
 (ns bio.file-io
   (:require [clojure.java.io :as io :refer [reader file]])
-  (:require [clojure.string :as s :refer [split]]))
+  (:require [clojure.string :as s :refer [split trim]]))
 
 (def data-columns [:last_name :first_name :gender :favorite_color :dob])
 (def record-delimiter (re-pattern "\\s[|]\\s|[,]\\s|\\s"))
 
-(defn assign-keys [line]
-  (->> (s/split line record-delimiter) 
-    (zipmap data-columns)))
+(defn assign-keys [result line]
+  (->> (s/split (trim line) record-delimiter) 
+    (zipmap data-columns)
+    (conj result)))
+
+(defn reduce-keys [coll]
+  (reduce assign-keys [] (filter #(not (empty? %)) coll)))
 
 (defn extract-records [file]
   (with-open [r (io/reader file)]
-    (->> (line-seq r)
-	 (map assign-keys))))
+   (reduce-keys (line-seq r))))
--- src/clj/test/bio/file_io_test.clj
+++ src/clj/test/bio/file_io_test.clj
@@ -1,15 +1,28 @@
 (ns bio.file-io-test
   (:require [clojure.test :refer :all]
+	    [clojure.string :as s :refer [split]]
             [bio.file-io :refer :all]))
 
+(deftest def-record-delimiter 
+  (let [result ["LastName" "FirstName" "Gender" "Color" "DOB"]]  
+  (testing "Regex correctly splits on comma" 
+      (let [comma "LastName, FirstName, Gender, Color, DOB"]  
+      (is (= result (s/split comma record-delimiter)))))
+  (testing "Regex correctly splits on space" 
+      (let [space "LastName FirstName Gender Color DOB"]  
+      (is (= result (s/split space record-delimiter)))))
+  (testing "Regex correctly splits on pipe" 
+      (let [pipe "LastName | FirstName | Gender | Color | DOB"]  
+      (is (= result (s/split pipe record-delimiter)))))))
+
 (deftest fn-assign-keys
-  (let [result (zipmap data-columns ["LastName" "FirstName" "Gender" "Color" "DOB"])]
+  (let [result (conj [] (zipmap data-columns ["LastName" "FirstName" "Gender" "Color" "DOB"]))]
   (testing "Assigns keys to a pipe delimited string" 
       (let [example "LastName | FirstName | Gender | Color | DOB"]  
-      (is (= result (assign-keys example)))))
+      (is (= result (assign-keys [] example)))))
   (testing "Assigns keys to a space delimited string" 
       (let [example "LastName FirstName Gender Color DOB"]  
-      (is (= result (assign-keys example)))))
+      (is (= result (assign-keys [] example)))))
   (testing "Assigns keys to a comma delimited string" 
       (let [example "LastName, FirstName, Gender, Color, DOB"]  
-      (is (= result (assign-keys example)))))))
+      (is (= result (assign-keys [] example)))))))
