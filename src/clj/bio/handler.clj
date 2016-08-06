e105b9b6923e2fd670a6b9730fd6ad4a9f0e4065
nrakochy <nick.rakochy@gmail.com>
2016-08-06 00:12:11 -0500

    Routes

--- src/clj/bio/handler.clj
+++ src/clj/bio/handler.clj
@@ -1,13 +1,20 @@
 (ns bio.handler
-  (:require [compojure.core :refer [GET defroutes]]
+  (:require [bio.core :refer [get-records update-records]]
+	    [compojure.core :refer [GET POST defroutes context]]
             [compojure.route :refer [resources]]
             [ring.util.response :refer [resource-response]]
+            [ring.middleware.params :refer [wrap-params]]
             [ring.middleware.reload :refer [wrap-reload]]))
 
 (defroutes routes
   (GET "/" [] (resource-response "index.html" {:root "public"}))
+  (context "/records" []
+    (POST "/" [req] (update-records req))
+    (GET "/gender" [] (get-records :gender))
+    (GET "/birthdate" [] (get-records :birthdate))
+    (GET "/name" [] (get-records :name)))
   (resources "/"))
 
-(def dev-handler (-> #'routes wrap-reload))
+(def dev-handler (-> #'routes wrap-reload wrap-params))
 
-(def handler routes)
+(def handler (-> routes wrap-params))
