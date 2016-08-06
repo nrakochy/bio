c354c1915878c1de9c6dae17470e4ed40001fb52
nrakochy <nick.rakochy@gmail.com>
2016-08-06 00:58:54 -0500

    Extracted sort configurations into single map

--- src/clj/bio/core.clj
+++ src/clj/bio/core.clj
@@ -1,1 +1,14 @@
 (ns bio.core)
+
+(def sort-configs {:gender {:order :asc :sym-key :gender}
+		   :lastname {:order :asc :sym-key :last_name}
+		   :dob {:order :asc :sym-key :dob}
+		   :lastname-desc {:order :asc :sym-key :last_name}})
+
+(defn get-records [sort-type]
+  (str "RETURNED" sort-type))
+  
+(defn update-records [req]
+  (str "REQUEST: " req))
+ 
+
--- src/clj/bio/project_reqs.clj
+++ src/clj/bio/project_reqs.clj
@@ -1,21 +1,20 @@
 (ns bio.project-reqs
-  (:require [bio.file-io :as fio :refer [extract-records multisort print-records]]))
+  (:require [bio.file-io :as fio :refer [extract-records multisort print-records]]
+	    [bio.core :refer [sort-configs]]))
 
-(def gender-asc {:order :asc :sym-key :gender})
-(def lastname-asc {:order :asc :sym-key :last_name})
-(def dob-asc {:order :asc :sym-key :dob})
-(def lastname-desc {:order :asc :sym-key :dob})
-(def project-reqs [[gender-asc lastname-asc] [dob-asc] [lastname-desc]]) 
+(defn required-sort [config]
+  (let [{:keys [gender lastname dob lastname-desc]} config]
+    [[gender lastname] [dob] [lastname-desc]]))
 
 (defn sort-print [coll sort-config]
   (let [separator "____________________________"]
   (prn separator)
-  (print-records (multisort coll sort-config))
+  (fio/print-records (fio/multisort coll sort-config))
   (prn separator)))
 
 (defn output-reqs [coll]
-  (dorun (map #(sort-print coll %) project-reqs)))
+  (dorun (map #(sort-print coll %) (required-sort sort-configs))))
 
-(defn run [& paths]
-  (output-reqs (extract-records paths)))
+(defn run [paths]
+  (output-reqs (fio/extract-records paths)))
 
